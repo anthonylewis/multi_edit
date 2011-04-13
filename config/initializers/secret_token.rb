4deffd87e1804274a685ce3bbf94d91787b647e9
Anthony Lewis <me@anthonylewis.com>
2011-04-13 16:09:43 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MultiEdit::Application.config.secret_token = '34b7b1d4a74813439d3d38d43c93dd200f805e02cf3469e5e658bbe12c2d2f03fc8b67aa6a844f4431cdd2f93c26c2330599be28284aa56cef4ccfba8a0abfd4'
